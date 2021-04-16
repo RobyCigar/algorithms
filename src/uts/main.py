37d774edacfff1ce7b3ab29b2b869426a17ee950
rabih <rabihutomo11@gmail.com>
2021-04-16 23:54:16 +0700

    reading some book

---
+++ src/uts/main.py
@@ -0,0 +1,3 @@
+kalimat="""Universitas Amikom Yogyakarta (UAYO) merupakan perubahan dari Sekolah Tinggi Manajemen Informatika dan Komputer Amikom Yogyakarta. Sebelumnya Sekolah Tinggi Manajemen Informatika dan Komputer Amikom Yogyakarta"""
+
+print(kalimat.split(" "))
